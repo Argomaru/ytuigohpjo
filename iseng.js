81a1df735df169e2d57c0b7e38e8dad6cc42417c
Argomaru <48087677+Argomaru@users.noreply.github.com>
2022-05-23 22:05:08 +0700

    Add files via upload

---
+++ iseng.js
@@ -0,0 +1,40 @@
+// GetDouble(4);
+
+// const GetDouble = function (numb) {
+//   console.log(2 * 2);
+// };
+
+// GetDouble(4);
+
+// function GetDouble(numb) {
+//   console.log(2 * 2);
+// }
+
+// const Object = 12;
+// console.log(Object);
+
+// console.log(typeof console.log);
+
+// function des() {
+//   let name = "asu";
+// }
+// console.log(name);
+
+// function waw(val, vab) {
+//   console.log(val * vab);
+// }
+// waw(2, 3);
+
+//ini salah
+// function asu(a, b) {
+//   return asu(a + b);
+// }
+// console.log(asu(2, 9));
+
+//ini bener (harus nambah var)
+function asu(a, b) {
+  let hasil = a + b;
+  return hasil;
+}
+
+console.log(asu(1, 2));
