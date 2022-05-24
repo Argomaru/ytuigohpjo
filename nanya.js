49fa50cc82f096fe898203198d17697fc87a458d
Argomaru <48087677+Argomaru@users.noreply.github.com>
2022-05-25 01:10:05 +0700

    Add files via upload

---
+++ nanya.js
@@ -0,0 +1,33 @@
+//21 nanya 
+//console.log(string(12) === "12")
+
+
+//19 kenapa ini bisa error
+//var fun = function bar{} //ini error
+//var fun = function bar(){}
+//var func=()=>{}
+//function fun(){}
+
+//18 nanya ini
+// let output = "hello"
+// if ("0" != true){
+//     output += ", world" 
+// }
+
+//5 nanya
+// GetDouble(4)
+
+// const GetDouble = function (numb){
+//     console.log(2*2)
+// }
+
+//3 nanya
+// let str = 'Javascript is for everyone yo!'.split(' ')
+
+// console.log(str)
+
+// for(let i = 0, i<str.length, i +=1){
+//     console.log(str[i])
+//     console.log(str)
+//     str.push(str[i])
+// }
